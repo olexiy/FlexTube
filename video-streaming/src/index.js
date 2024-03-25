c8b65c35da2ddbea92cefb6fc501e36a7ad70265
Olexiy Sokurenko <ol.sakura@gmail.com>
2024-03-25 21:44:17 +0000

    Refactor video streaming endpoint to forward requests to a separate video storage service

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const http = require("http");
- 
-const app = express();
+
+const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = process.env.VIDEO_STORAGE_PORT;
 
 if (!process.env.PORT) {
   throw new Error("PORT environment variable is not set");
 }
 
-const PORT = process.env.PORT;
- 
+const app = express();
+
 app.get("/video", async (req, res) => {
-  const videoPath =  "./videos/SampleVideo_1280x720_1mb.mp4";
-  const stats = await fs.promises.stat(videoPath);
- 
-  res.writeHead(200, {
-    "Content-Length": stats.size,
-    "Content-Type": "video/mp4",
-  });  
-  fs.createReadStream(videoPath).pipe(res);
+
+  const forwardRequest = http.request(
+    {
+      host: VIDEO_STORAGE_HOST,
+      port: VIDEO_STORAGE_PORT,
+      path: "/files/SampleVideo_1280x720_1mb.mp4",
+      method: "GET",
+      headers: req.headers,
+    },
+    (forwardResponse) => {
+      res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+      forwardResponse.pipe(res);
+    }
+  );
+
+  req.pipe(forwardRequest);
 });
- 
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

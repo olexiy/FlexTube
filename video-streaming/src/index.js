f0647e7cdc8cb1d57fa7d6f7afb9ec696bdffa75
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2024-03-26 12:52:15 +0100

    Add videos.json fixture file and update package.json and docker-compose.yml. DB lookup works now

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,37 +1,61 @@
 const express = require("express");
 const http = require("http");
+const mongodb = require("mongodb");
 
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = process.env.VIDEO_STORAGE_PORT;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
 
-if (!process.env.PORT) {
-  throw new Error("PORT environment variable is not set");
+async function main() {
+  const client = await mongodb.MongoClient.connect(DB_HOST);
+  const db = client.db(DB_NAME);
+  const videosCollection = db.collection("videos");
+
+  if (!process.env.PORT) {
+    throw new Error("PORT environment variable is not set");
+  }
+
+  const app = express();
+
+  app.get("/video", async (req, res) => {
+    const videoId = new mongodb.ObjectId(req.query.id);
+    console.log(`Mongo findOne for video ID: ${videoId}.`);
+    const videoRecord = await videosCollection.findOne({ _id: videoId });
+    if (!videoRecord) {
+      res.status(404).send("Video not found");
+      return;
+    }
+
+    console.log(
+      `Request forwarded to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}`
+    );
+
+    const forwardRequest = http.request(
+      {
+        host: VIDEO_STORAGE_HOST,
+        port: VIDEO_STORAGE_PORT,
+        path: `/files/${videoRecord.videoPath}`,
+        method: "GET",
+        headers: req.headers,
+      },
+      (forwardResponse) => {
+        res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+        forwardResponse.pipe(res);
+      }
+    );
+
+    req.pipe(forwardRequest);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Video Streaming Server is running on port ${PORT}`);
+  });
 }
 
-const app = express();
+main().catch((err) => {
+  console.error("Microservice failed to start", err);
 
-app.get("/video", async (req, res) => {
-  console.log(
-    `Request forwarded to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}`
-  );
-  const forwardRequest = http.request(
-    {
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: "/files/SampleVideo_1280x720_1mb.mp4",
-      method: "GET",
-      headers: req.headers,
-    },
-    (forwardResponse) => {
-      res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
-      forwardResponse.pipe(res);
-    }
-  );
-
-  req.pipe(forwardRequest);
-});
-
-app.listen(PORT, () => {
-  console.log(`Video Streaming Server is running on port ${PORT}`);
+  process.exit(1);
 });
